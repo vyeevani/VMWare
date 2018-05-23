8b6e8cb0283efeca57fbdbee58d5cda2ebe6db43
vineethyeevani <vyeevani@berkeley.edu>
2018-05-22 22:33:47 -0700

    Added generating capability

--- image_captioning/experimental/second_attempt/main.py
+++ image_captioning/experimental/second_attempt/main.py
@@ -1,19 +1,38 @@
-from utils.train import train
-from utils.get_data import get_data
+import os
+import tensorflow as tf
+import numpy as tf
 
 model_path = './models/tensorflow'
-model_path_transfer = './models/tf_final'
-feature_path = './data/feats.npy'
-annotation_path = './data/results_20130124.token'
-print(get_data)
-feats, captions = get_data(annotation_path, feature_path)
-print(feats.shape)
-print(captions.shape)
-print(captions[0])
+vgg_path = './data/vgg16-20160129.tfmodel'
+image_paths = ['./data/images/img1', './data/image/img2']
 
-try:
-    train(.001,False,False) #train from scratch
-    #train(.001,True,True)    #continue training from pretrained weights @epoch500
-    #train(.001,True,False)  #train from previously saved weights
-except KeyboardInterrupt:
-    print('Exiting Training')
+dim_embeddim_emb  = 256
+dim_hidden = 256
+dim_in = 4096
+batch_size = 1
+learning_rate = 0.001
+momentum = 0.9
+n_epochs = 25
+
+if not os.path.exists('data/ixtoword.npy'):
+    print ('You must run 1. O\'reilly Training.ipynb first.')
+else:
+    tf.reset_default_graph()
+    with open(vgg_path,'rb') as f:
+        fileContent = f.read()
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(fileContent)
+
+    images = tf.placeholder("float32", [1, 224, 224, 3])
+    tf.import_graph_def(graph_def, input_map={"images":images})
+
+    ixtoword = np.load('data/ixtoword.npy').tolist()
+    n_words = len(ixtoword)
+    maxlen=15
+    graph = tf.get_default_graph()
+    sess = tf.InteractiveSession(graph=graph)
+    caption_generator = Caption_Generator(dim_in, dim_hidden, dim_embed, batch_size, maxlen+2, n_words)
+    graph = tf.get_default_graph()
+    image, generated_words = caption_generator.build_generator(maxlen=maxlen)
+    for image_path in image_paths:
+        test(sess,image,generated_words,ixtoword, image_path)
---
+++ image_captioning/experimental/second_attempt/utils/test.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+
+def test(sess,image,generated_words,ixtoword,test_image_path=0): # Naive greedy search
+    feat = read_image(test_image_path)
+    fc7 = sess.run(graph.get_tensor_by_name("import/Relu_1:0"), feed_dict={images:feat})
+
+    saver = tf.train.Saver()
+    sanity_check=False
+    # sanity_check=True
+    if not sanity_check:
+        saved_path=tf.train.latest_checkpoint(model_path)
+        saver.restore(sess, saved_path)
+    else:
+        tf.global_variables_initializer().run()
+
+    generated_word_index= sess.run(generated_words, feed_dict={image:fc7})
+    generated_word_index = np.hstack(generated_word_index)
+    generated_words = [ixtoword[x] for x in generated_word_index]
+    punctuation = np.argmax(np.array(generated_words) == '.')+1
+
+    generated_words = generated_words[:punctuation]
+    generated_sentence = ' '.join(generated_words)
+    print(generated_sentence)
