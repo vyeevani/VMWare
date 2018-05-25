918760b6cd619fec20091eb5a12ff66c79b7e8b6
vineethyeevani <vyeevani@berkeley.edu>
2018-05-25 15:05:16 -0500

    Finished laying out the website

---
+++ website/assets/js/graph.js
@@ -0,0 +1,54 @@
+// Set the dimensions of the canvas / graph
+var	margin = {top: 30, right: 20, bottom: 30, left: 50},
+	width = 600 - margin.left - margin.right,
+	height = 270 - margin.top - margin.bottom;
+
+// Parse the date / time
+//var	parseDate = d3.time.format("%d-%b-%y").parse;
+
+// Set the ranges
+var	x = d3.scale.linear().range([0, width]);
+var	y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+var	xAxis = d3.svg.axis().scale(x)
+	.orient("bottom").ticks(5);
+
+var	yAxis = d3.svg.axis().scale(y)
+	.orient("left").ticks(5);
+
+// Define the line
+var	valueline = d3.svg.line()
+	.x(function(d) { return x(d.iteration); })
+	.y(function(d) { return y(d.loss); });
+
+// Adds the svg canvas
+var	svg = d3.select("#stock")
+	.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("img/outputfile.csv", function(error, data) {
+	// Scale the range of the data
+	x.domain([0, 28]);
+	y.domain([0, d3.max(data, function(d) { return d.loss; })]);
+
+	// Add the valueline path.
+	svg.append("path")
+		.attr("class", "line")
+		.attr("d", valueline(data));
+
+	// Add the X Axis
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+	// Add the Y Axis
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis);
+});
