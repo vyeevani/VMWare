37bc84cb323c21d55896ae74c67f5720c3c37153
vineethyeevani <vyeevani@berkeley.edu>
2018-06-05 12:39:14 -0700

    Changed training cheg checkpoints

--- distributed_mnist/experimental/second_attempt/utils/train.py
+++ distributed_mnist/experimental/second_attempt/utils/train.py
@@ -43,7 +43,7 @@ def train(learning_rate=0.001, continue_training=False, transfer=True, server=No
         train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
 
-        with tf.train.MonitoredTrainingSession(server.target, is_chief=is_chief, hooks=hooks) as sess:
+        with tf.train.MonitoredTrainingSession(server.target, checkpoint_dir="/ml_data/Distributed_Model/training_checkpoints", is_chief=is_chief, hooks=hooks) as sess:
             #sess.run(tf.global_variables_initializer())
 
             if continue_training:
@@ -79,4 +79,4 @@ def train(learning_rate=0.001, continue_training=False, transfer=True, server=No
                     print("Current Cost: ", loss_value, "\t Epoch {}/{}".format(epoch, n_epochs), "\t Iter {}/{}".format(start,len(feats)))
                 print("Saving the model from epoch: ", epoch)
                 epoch += 1
-                saver.save(sess, os.path.join(model_path, 'model'), global_step=epoch)
+                #saver.save(sess, os.path.join(model_path, 'model'), global_step=epoch)
